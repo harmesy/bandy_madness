3812c5138a9465fbdb1636003f9c08a66bd31880
Matt Harmes <matt@harmes.ca>
2016-03-15 01:11:17 -0400

    UI updates.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def output_selections(selections, &block)
+    if selections.any?
+      selections.each do |selection|
+        block.call(selection)
+      end
+    else
+      concat("You have not made a selection for this game.")
+    end
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ActiveRecord::Base
   belongs_to :round
   belongs_to :team_one, class_name: 'Team'
   belongs_to :team_two, class_name: 'Team'
+  has_many :selections
 end
