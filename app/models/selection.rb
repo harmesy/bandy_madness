da2abc887b47383c62c99007cbec916a160d9179
Matt Harmes <matt@harmes.ca>
2016-03-15 10:37:55 -0400

    Make sure each user can only make one selection per game.
    Not going to move to multi-column primary keys or adding a unique index
    to [user, game] because I think I’m going to edit this shortly to
    preserve history of selections.

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -2,4 +2,14 @@ class Selection < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
   belongs_to :team
+
+  validate :one_selection_per_user_per_game
+
+  protected
+
+  def one_selection_per_user_per_game
+    if Selection.where(user: user, game: game).exists?
+      errors[:game] = "You have already made a selection for this game."
+    end
+  end
 end
