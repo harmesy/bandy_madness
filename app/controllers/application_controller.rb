30dff19c5e9c47d16fd6793cf7b888fb9ea8da53
Matt Harmes <matt@harmes.ca>
2016-03-15 23:07:49 -0400

    Need to be logged in to see anything.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :get_rounds
+  before_action :authenticate_user!
+  before_action :populate_sidebar
 
   private
 
-  def get_rounds
+  def populate_sidebar
+    return unless current_user
     @rounds = Round.all
   end
 end
